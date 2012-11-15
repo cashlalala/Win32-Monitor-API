060047a84afc64e33cd56f3c64dbdc5136e36167
cash <cash924n@gmail.com>
2012-11-16 03:48:02 +0800

    Basic win32 monitor detection is done

--- Win32MultiMonitorDemo/MainWindow.xaml.cs
+++ Win32MultiMonitorDemo/MainWindow.xaml.cs
@@ -12,6 +12,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Win32MultiMonitorDemo.Util;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+using System.Windows.Interop;
+
 namespace Win32MultiMonitorDemo
 {
     /// <summary>
@@ -22,6 +29,59 @@ namespace Win32MultiMonitorDemo
         public MainWindow()
         {
             InitializeComponent();
+
+            winHelper = new WindowInteropHelper(this);
+
+            this.dispatchTimer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, OnDispatcherTimer, this.Dispatcher);
+
+        }
+
+        private DispatcherTimer dispatchTimer;
+
+        private WindowInteropHelper winHelper;
+
+        private static int count = 0;
+
+        private void OnDispatcherTimer(object sender, EventArgs e)
+        {
+            Win32.MultiMonitorHelper.MONITORINFOEX monInfoEx = new Win32.MultiMonitorHelper.MONITORINFOEX();
+           
+            /*
+             * Get the monitor info from point
+             */
+            //Win32.MultiMonitorHelper.POINTSTRUCT pt = new Win32.MultiMonitorHelper.POINTSTRUCT(2000, 500);
+            //Win32.GeneralSafeHandle hMon = Win32.MultiMonitorHelper.MonitorFromPoint(pt, Win32.MultiMonitorHelper.MONITOR_DEFAULTTONEAREST);
+            //bool result = Win32.MultiMonitorHelper.GetMonitorInfo(hMon, monInfoEx);
+
+            /*
+             * Get the monitor info from window 
+             */
+            Win32.GeneralSafeHandle hMonFromWin = Win32.MultiMonitorHelper.MonitorFromWindow(winHelper.Handle, Win32.MultiMonitorHelper.MONITOR_DEFAULTTONEAREST);
+
+            bool result = Win32.MultiMonitorHelper.GetMonitorInfo(hMonFromWin, monInfoEx);
+
+
+            if (result)
+            {
+                this.monInfo.a1.Text = new String(monInfoEx.szDevice).TrimEnd('\0') + (++count).ToString();
+                this.monInfo.a2.Text = String.Format("handle in winhelp : {0:X}",winHelper.Handle);
+                //this.monInfo.a3.Text = String.Format("handle in windowHandle : {0:X}", );
+            }
+            else
+            {
+                //throw new Win32Exception(Marshal.GetLastWin32Error());
+                this.monInfo.a1.Text = String.Format("Get error code: {0}", Marshal.GetLastWin32Error());
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (this.dispatchTimer.IsEnabled)
+            {
+                this.dispatchTimer.Stop();
+            }
         }
     }
+
+
 }
---
+++ Win32MultiMonitorDemo/Util/Win32.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.ConstrainedExecution;
+using Microsoft.Win32.SafeHandles;
+
+namespace Win32MultiMonitorDemo.Util
+{
+    public static class Win32
+    {
+        public class GeneralSafeHandle : SafeHandle
+        {
+
+            private GeneralSafeHandle()
+                : base(IntPtr.Zero, //invalid value
+                         true)           //is released at the end of SafeHandle
+            {
+            }
+
+            public GeneralSafeHandle(IntPtr hHandle)
+                : base(IntPtr.Zero,true)
+            {
+                SetHandle(hHandle);
+            }
+
+            [ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+            protected override bool ReleaseHandle()
+            {
+                // If ReleaseHandle failed, it can be reported via the
+                // "releaseHandleFailed" managed debugging assistant (MDA).  This
+                // MDA is disabled by default, but can be enabled in a debugger
+                // or during testing to diagnose handle corruption problems.
+                // We do not throw an exception because most code could not recover
+                // from the problem.
+                return Win32.CloseHandle(this.handle);
+            }
+
+            public static implicit operator IntPtr (GeneralSafeHandle generalSafeHandle)
+            {
+                return generalSafeHandle.handle;
+            }
+
+            //public static explicit operator IntPtr(GeneralSafeHandle generalSafeHandle)
+            //{
+            //    return generalSafeHandle.handle;
+            //}
+
+            //public static explicit operator HandleRef(GeneralSafeHandle generalSafeHandle)
+            //{
+            //    return new HandleRef(generalSafeHandle, generalSafeHandle.handle);
+            //}
+
+            public static implicit operator HandleRef(GeneralSafeHandle generalSafeHandle)
+            {
+                return new HandleRef(generalSafeHandle, generalSafeHandle.handle);
+            }
+
+
+            public override bool IsInvalid
+            {
+                get { return IsClosed || handle == IntPtr.Zero; }
+            }
+        }
+
+        [DllImport("kernel32", SetLastError = true)]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private extern static bool CloseHandle(IntPtr handle);
+
+
+        public static class MultiMonitorHelper
+        {
+            [StructLayout(LayoutKind.Sequential,CharSet=CharSet.Auto, Pack=4)]
+            public class MONITORINFOEX { 
+                public int     cbSize = Marshal.SizeOf(typeof(MONITORINFOEX));
+                public RECT    rcMonitor = new RECT(); 
+                public RECT    rcWork = new RECT(); 
+                public int     dwFlags = 0;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst=32)] 
+                public char[]  szDevice = new char[32];
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct POINTSTRUCT { 
+                public int x;
+                public int y;
+                public POINTSTRUCT(int x, int y) {
+                  this.x = x; 
+                  this.y = y;
+                } 
+            } 
+
+            [StructLayout(LayoutKind.Sequential)] 
+            public struct RECT {
+                public int left; 
+                public int top; 
+                public int right;
+                public int bottom; 
+            }
+
+            [DllImport("User32.dll", SetLastError = true,CharSet=CharSet.Auto)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetMonitorInfo(HandleRef hMonitor, [In, Out] Win32MultiMonitorDemo.Util.Win32.MultiMonitorHelper.MONITORINFOEX monitorInfo);
+
+            [DllImport("User32.dll", SetLastError = true)]
+            public static extern GeneralSafeHandle MonitorFromPoint(Win32MultiMonitorDemo.Util.Win32.MultiMonitorHelper.POINTSTRUCT pt, uint flags);
+
+
+            /*
+             * The window handle you found no need to be closed manually,
+             * so here only use the IntPtr as the Input type.
+             * HandleRef can also be used here.
+             */
+            [DllImport("User32.dll",SetLastError=true)]
+            public static extern GeneralSafeHandle MonitorFromWindow(IntPtr hWnd, uint dwFlags);
+
+
+            public const uint MONITOR_DEFAULTTONULL = 0;
+            public const uint MONITOR_DEFAULTTOPRIMARY = 1;
+            public const uint MONITOR_DEFAULTTONEAREST = 2;
+        
+        }
+
+
+    }
+}
